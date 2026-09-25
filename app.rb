7236438036a7294215f5ebf4811240c8749bbb2d
Joon Ahn <sa3883@columbia.edu>
2026-09-25 23:13:29 +0000

    Error fixed

--- app.rb
+++ app.rb
@@ -40,7 +40,15 @@ class WordGuesserApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    @game.guess(letter)
+
+    begin
+      if @game.guess(letter) == false
+        flash[:message] = 'You have already used that letter.'
+      end
+    rescue ArgumentError
+      flash[:message] = 'Invalid guess.'
+    end
+
     redirect '/show'
   end
 
